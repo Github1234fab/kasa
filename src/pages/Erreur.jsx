227356313ea26e4208aa473e202585957b011e84
Fabien Marceau <fabien.marceau@gmail.com>
2022-12-19 14:27:15 +0100

    erreurPage ok

--- src/pages/Erreur.jsx
+++ src/pages/Erreur.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 import "./Erreur.css";
+import { Link } from 'react-router-dom';
 
 const Erreur = () => {
     return (
         <div>
-          Erreur 
-          <Navbar /> 
+          <div className='Erreur_visuels'>
+          <div className = "Erreur_404">404</div>
+          <div className = "Erreur_oups">Oups! la page que vous demandez n'existe pas.</div>
+        <Link to={"./"} className = "Erreur_retour">Retournez sur la page d'accueil</Link> 
         </div>
+        </div>      
     );
 };
 
